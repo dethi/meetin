3be429d6f7c41ef26af30c01dbbf048753c5775f
Kevin Louzoun <kevin.louzoun@epita.fr>
2017-05-26 23:42:22 +0200

    src/config src/helpers/ : begin to setup Firebase

---
+++ frontend/src/config/constant.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyCr5XQgRPaTD6WDJtbXREwGNMLF_lyrxKo',
+  authDomain: 'jsproject-34dfd.firebaseapp.com',
+  databaseURL: 'https://jsproject-34dfd.firebaseio.com',
+  projectId: 'jsproject-34dfd',
+  storageBucket: 'jsproject-34dfd.appspot.com',
+  messagingSenderId: '303524053697'
+}
+
+firebase.initializeApp(config)
+
+export const ref = firebase.database().ref()
+export const firebaseAuth = firebase.auth
