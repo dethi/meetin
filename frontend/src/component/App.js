7134a582923eca4a6e30e86836153e3aa3517269
Kevin Louzoun <kevin.louzoun@epita.fr>
2017-05-27 00:27:28 +0200

    frontend/src/component/Login/Forget.css : add forget email

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Index from './Index/Index'
 import Login from './Login/Login'
+import Forget from './Login/Forget'
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ const App = () => (
     <div>
       <Route exact path="/" component={Index} />
       <Route path="/login" component={Login} />
+      <Route path="/forget" component={Forget} />
     </div>
   </Router>
 )
