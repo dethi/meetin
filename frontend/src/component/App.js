555091660781f8eca41fd6a05100d1334b33f77e
Kevin Louzoun <kevin.louzoun@epita.fr>
2017-05-26 22:03:55 +0200

    src/component/App.js: Add ReactRouterDom

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Index from './Index/Index'
 import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => (
-  <div>
-    <Index />
-  </div>
+  <Router>
+    <div>
+      <Route path="/" component={Index} />
+      <Route path="/login" component={Index} />
+    </div>
+  </Router>
 )
 
 export default App
