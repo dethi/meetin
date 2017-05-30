d5935bc0b4c0da136448bf7b1ea3bc6ae89f8bc1
Florent Youinou <youino_f@epita.fr>
2017-05-30 21:47:22 +0200

    actu: add listing events

--- frontend/src/component/Dashboard/index.js
+++ frontend/src/component/Dashboard/index.js
@@ -1,26 +1,128 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import NavBar from './../Navbar';
 import TitleBar from './../TitleBar';
 
 import emptyEvents from './../../img/events-empty-data-set.png';
 
+class EventItem extends Component {
+  render() {
+    const outdated = {
+      opacity: '0.6',
+      filter: 'brightness(90%)'
+    };
+
+    return (
+      <div className="box" style={this.props.outdated ? outdated : null}>
+        <article className="media">
+          <div className="media-left is-vcentered">
+            <figure className="image is-128x128" style={{ padding: '20px' }}>
+              <img src={this.props.icon} alt="Image" />
+            </figure>
+          </div>
+          <div className="media-content is-vcentered">
+            <div className="content">
+              <p>
+                <span className="title is-3">
+                  <strong>{this.props.name}</strong>
+                </span>
+                {' '}
+                <span
+                  className="icon is-small"
+                  style={{ marginLeft: '10px', marginRight: '5px' }}
+                >
+                  <i className="fa fa-calendar" aria-hidden="true" />
+                </span>
+                <span>
+                  <small>{this.props.date}</small>
+                </span>
+                <span className="hspaced">-</span>
+                <small>{this.props.time}</small>
+              </p>
+              <p>{this.props.description}</p>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [
+        {
+          id: 1,
+          name: 'Pizza',
+          date: '11/10/2017',
+          time: 'Midi',
+          description: 'Petite pizza oklm',
+          icon: 'https://image.flaticon.com/icons/svg/167/167741.svg',
+          outdated: false
+        },
+        {
+          id: 1,
+          name: 'Basket',
+          date: '11/10/2017',
+          time: 'Midi',
+          description: 'Avec la team',
+          icon: 'https://image.flaticon.com/icons/svg/167/167739.svg',
+          outdated: false
+        },
+        {
+          id: 1,
+          name: 'Etudes',
+          date: '11/10/2017',
+          time: 'Midi',
+          description: 'Avec la team',
+          icon: 'https://image.flaticon.com/icons/svg/167/167729.svg',
+          outdated: true
+        },
+        {
+          id: 1,
+          name: 'Salad party',
+          date: '11/10/2017',
+          time: 'Midi',
+          description: 'Un esprit sain dans un corps sain',
+          icon: 'https://image.flaticon.com/icons/svg/167/167759.svg',
+          outdated: true
+        },
+        {
+          id: 1,
+          name: 'Kung fu',
+          date: '11/10/2017',
+          time: 'Midi',
+          description: 'Un esprit sain dans un corps sain',
+          icon: 'https://image.flaticon.com/icons/svg/167/167752.svg',
+          outdated: true
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
         <NavBar menuActive="home" />
-        <TitleBar title="Mon actu" />
-        <div className="container">
-          <figure className="container has-text-centered">
-            <img src={emptyEvents} alt="profile_picture" />
-          </figure>
+        <TitleBar title="Mon actualité" />
+        <div className="columns">
+          <div className="column is-8-tablet is-10-mobile is-center">
+            {!this.state.events.length
+              ? <div>
+                  <figure className="has-text-centered">
+                    <img src={emptyEvents} alt="profile_picture" />
+                  </figure><p className="subtitle is-4 has-text-centered">
+                    Oops, vous n'avez aucun evenement..
+                  </p>
+                </div>
+              : this.state.events.map((e, i) => {
+                  return <EventItem key={i} {...e} />;
+                })}
+          </div>
         </div>
-        <p className="subtitle is-3 has-text-centered">
-          Oops, vous n'avez aucun evenement..
-        </p>
-        <p className="subtitle is-5 has-text-centered">
-          Rejoindre un evenement
-        </p>
       </div>
     );
   }
