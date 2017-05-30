b5e2150825b622a6ddd823f562ceeeb79fccd9f4
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-05-30 23:46:29 +0200

    auth: add loading component

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import firebase from 'firebase';
+
+import Loading from './Loading';
 import Index from './Index';
 import Login from './Login';
 import Dashboard from './Dashboard';
@@ -6,37 +10,70 @@ import Profile from './Profile';
 import Event from './Events/Item';
 import Team from './Team';
 import Evenement from './Evenement';
+
 import { isAuthenticated } from '../utils';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      (isAuthenticated()
+      isAuthenticated()
         ? <Component {...props} />
         : <Redirect
             to={{
               pathname: '/login',
               state: { from: props.location }
             }}
-          />)}
+          />}
   />
 );
 
-const App = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={Index} />
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/evenement" component={Evenement} />
-      <Route path="/event/:id" component={Event} />
-      <Route path="/team/:id" component={Team} />
-    </div>
-  </Router>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      isLogged: false,
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          isLogged: true,
+          isLoading: false
+        });
+      } else {
+        this.setState({
+          isLogged: false,
+          isLoading: false
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    return isLoading
+      ? <Loading />
+      : <Router>
+          <div>
+            <Route exact path="/" component={Index} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <PrivateRoute path="/profile" component={Profile} />
+            <PrivateRoute path="/evenement" component={Evenement} />
+            <PrivateRoute path="/event/:id" component={Event} />
+            <PrivateRoute path="/team/:id" component={Team} />
+          </div>
+        </Router>;
+  }
+}
---
+++ frontend/src/component/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => (
+  <section className="hero is-primary is-fullheight">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <span className="icon is-large">
+          <i className="fa fa-spinner fa-pulse" />
+        </span>
+      </div>
+    </div>
+  </section>
+);
+
+export default Loading;
