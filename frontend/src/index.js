8d6bf3431b0fdc62f11760bae8acc0b3d2814f56
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-05-31 00:16:51 +0200

    redux: configure react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 import './bootstrap';
 
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(<Root />, rootEl);
 registerServiceWorker();
