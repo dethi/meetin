165117d1757bdb34fd50d2911d4ea3b0ea20ae68
Florent Youinou <youino_f@epita.fr>
2017-06-10 19:02:53 +0200

    generic group length

--- frontend/src/components/Evenement/index.js
+++ frontend/src/components/Evenement/index.js
@@ -97,17 +97,13 @@ class Evenement extends Component {
                                 className="select--property"
                                 onChange={this.handleChangeSelected}
                               >
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
+                                {Array.apply(null, { length: 11 })
+                                  .map(Number.call, Number)
+                                  .map(i => {
+                                    return (
+                                      <option value={i + 2}>{i + 2}</option>
+                                    );
+                                  })}
                               </select>
                             </span>
                             <span className="icon is-small is-left">
